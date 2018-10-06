93f09ec0009d192301a0a72c335d852a68c6e9cf
XiaoxiaoDDD <38062834+XiaoxiaoDDD@users.noreply.github.com>
2018-10-07 01:28:32 +0400

    finish~

--- dictionary.h
+++ dictionary.h
@@ -5,7 +5,6 @@
 #include <fstream>
 #include <vector>
 #include <utility>
-#include <math.h>
 #include <string>
 
 //class prototype
@@ -15,13 +14,18 @@ private:
 	std::string input;
 	std::vector<std::string> & dictionary;
 	int size;
+	int n_comparison;
+	int n_word_found;
 
 public:
 	Enquiry(std::string, std::vector<std::string> &);
 	std::pair<char, int> pre_process();
 	void myspeller (char, int);
-	void complete_search(int &, int &, std::vector<std::string *>&);
-	void wildcard_search (int &, int &, std::vector<std::string *>&, int);
+	void binary_search( std::vector<std::string *>&);
+	//void wildcard_search ( std::vector<std::string *>&, int, char &);
+	void incomplete_search( std::vector<std::string *>&, int, char &);
+	void find_first_occurance(int & , int & );
+	void find_last_occurance(int & , int & );
 
 
 	~Enquiry();
--- enquiry.cpp
+++ enquiry.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 Enquiry::Enquiry(std::string input, std::vector<std::string> & dic)
-		:input(input), dictionary(dic){
+		:input(input), dictionary(dic), n_word_found(0), n_comparison(0){
 		size = dictionary.size();
 
 		std::pair<char, int> instruction = pre_process(); //determine the type of enquiry: complete word, wildcard, or ?
@@ -34,25 +34,17 @@ std::pair<char, int> Enquiry::pre_process() {
 
 void Enquiry::myspeller(char c, int p){  //c is the enquiry type, p is the position where * and ? appear
 	std::vector<std::string *> result_list;
-	int n_comparison;
-	n_comparison = 0;
-	int n_word_found;
-	n_word_found = 0;
+
 	if (c == '\0'){
-		complete_search(n_word_found, n_comparison, result_list);
+		binary_search(result_list);
 	}
 
 	else if (c =='*'){
-		wildcard_search(n_word_found, n_comparison, result_list, p);
-		
-
+		incomplete_search(result_list, p, c);		
 	}
 
 	else if (c == '?'){
-		;
-		// here insert codes that deal with enquires with ?
-
-
+		incomplete_search(result_list, p, c);
 	}
 			
 
@@ -68,87 +60,7 @@ void Enquiry::myspeller(char c, int p){  //c is the enquiry type, p is the posit
 }
 
 		
-void Enquiry::complete_search(int & n_word_found, int & comparison_count, std::vector<std::string *> & result_list){
-	int left;
-	int right;
-	int middle;
-	int index_found;
-	index_found = -1;
-	left = 0;
-	right = size - 1;
-	
-	while (left <= right) {
-		comparison_count++;
-		middle = (left + right)/ 2;
 
-		if (input.compare(dictionary[middle]) > 0) {
-			left = middle + 1;
-		}
-		else if (input.compare(dictionary[middle]) < 0) {
-			right = middle - 1;
-		}
-		else {
-			index_found = middle;	
-			// std::cout <<"found!!!yes!!" <<std::endl;
-			break;
-		}
-	}
-
-	if (index_found == -1){
-		n_word_found = 0;
-	}
-	else{
-		// std::cout <<"the index is "<<index_found << std::endl;
-		result_list.push_back(&dictionary[middle]);
-		n_word_found = 1;
-	}
-
-}
-
-void Enquiry::wildcard_search(int & n_word_found, int & comparison_count, std::vector<std::string*> & result_list, int p){
-
-	int first_occurance, last_occurance;
-	first_occurance = -1;
-	last_occurance = -1;
-	int left;
-	int right;
-	int middle;
-	left = 0;
-	right = size - 1;
-
-	
-	while (left <= right) {
-		comparison_count++;
-		middle = (left + right)/ 2;
-
-		if (input.substr(0,p).compare(dictionary[middle].substr(0,p)) > 0) {
-			left = middle + 1;
-		}
-		else if (input.substr(0,p).compare(dictionary[middle].substr(0,p)) <= 0) {
-			right = middle;
-		}
-		if (left == right) break;
-	}
-	if (left == right && dictionary[left].substr(0,p) == input.substr(0,p)){
-		first_occurance = left;
-	}
-
-	if (first_occurance != -1){
-		int dynamic_index;
-		dynamic_index = first_occurance;
-		while (dictionary[dynamic_index].substr(0,p) == input.substr(0,p)){
-			comparison_count++;
-			n_word_found++;
-			result_list.push_back(&dictionary[dynamic_index]);
-			dynamic_index++;
-		}
-
-	}
-
-}
-
-
-	
 Enquiry::~Enquiry(){
 	;
 }
